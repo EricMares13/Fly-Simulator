9dc424fe4613f60060567daa38bca8984b935b01
Èric Marès <eric.mares@mataro.epiaedu.cat>
2016-10-26 20:40:51 +0200

    Segon commit
    Començar a fer la introducció d'avions i mostrar l'espai aeri

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,14 +1,61 @@
+import java.util.Scanner;
 
 public class Controlador {
-
+	
+	static Scanner sc;
+	static EspaiAeri espai;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int i;
+		//a1=new Aula(1,"Aula Química",2);
+		espai = new EspaiAeri("Espai aeri asd", 5);
+		do{
+			System.out.println("Entra una opció de les següents");
+			System.out.println("1.- Dona d'alta un avió");
+			System.out.println("2.- Quin avió vols gestionar");
+			System.out.println("3.- Manteniment de l'espai aeri");
+			System.out.println("4.- Informació de l'espai aeri");
+			System.out.println("0.- Sortir");
+			
+			sc = new Scanner(System.in);
+		    i = sc.nextInt();
+		    switch (i)
+		    {
+		    case 1:  break;
+		    	
+		    case 2:  break;
+		    	
+		    case 3:  break;
+		    
+		    //case 4: infoEspaiAeri(); break;
+		    	
+		    }
+		}while (i!=0);
+		
+		
+	}	
 		
 		
 		
 		
+		/*public static void infoEspaiAeri(){
+			System.out.println("Entra La matrícula a buscar:");
+			String MatriculaBuscar=sc.next();
+			sc.nextLine();
+			a1.buscarAlumne(MatriculaBuscar);
+		}*/
 		
+	public static void altaAvio (){
+		String model, matricula, marca;
+		int passatges=0;
+		int posX, posY;
+		
+		espai.afegirAvions(model, matricula, marca, passatges, posX, posY);
 	}
+		
+		
+		
+	
 
 }
--- src/EspaiAeri.java
+++ src/EspaiAeri.java
@@ -1,4 +1,51 @@
 
 public class EspaiAeri {
 
+	private Avions LlistaAvions[];
+	private int numAvions=0;
+	String nomEspaiAeri;
+	
+	public EspaiAeri(String NomEspaiAeri, int numeroAvions){
+		this.numAvions = numeroAvions;
+		this.nomEspaiAeri = NomEspaiAeri;
+		
+		LlistaAvions = new Avions[numeroAvions];
+		
+	}
+	
+	private int getnumAvions(){
+		return numAvions;
+	}
+	
+	
+	
+	
+	public void afegirAvions(String matr, String mod, String marc, int passatges, int posX, int posY)
+	{ Avions CreacioAvio =new Avions(matr,mod,marc, passatges, posX, posY);
+	
+	  
+		if(LlistaAvions[numAvions]==null){
+			LlistaAvions[numAvions]=CreacioAvio;
+			  numAvions++;
+			  	
+		}
+			
+		for(numAvions=0; numAvions<=LlistaAvions.length;numAvions++){
+			if(LlistaAvions[numAvions].getMatricula().equals(matr)){
+				System.out.println("Matricula ja introduida");
+				break;
+				
+			}
+			
+			if(LlistaAl[numAl]==null){
+				LlistaAl[numAl]=CreacioAvio;
+				numAl++;
+				break;
+			}
+		}
+	  
+	  
+	  
+	  
+	}
 }
